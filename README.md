319ff1a01c2722df3fbc32562d64eacdaa2ec2ea
Gaurav Bhatnagar <gbhatnagar@berkeley.edu>
2023-08-04 14:30:42 -0700

    fix: accurate linkPath handling

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # remark-d2
-A remark plugin for compiling d2 code blocks in markdown
+
+A remark plugin for compiling d2 code blocks in Markdown files, allowing you to do this:
+
+
+
+## Installation
+
+Ensure that you have d2 installed and accessible on your PATH. Then, install using your favorite package manager:
+
+`npm i -D @mech-a/remark-d2`
+
+## Usage
+
+In your unified or remark toolchain, you can include the plugin:
+
+## Options
+
+## Integrations
+
+## Inspiration
--- index.js
+++ index.js
@@ -6,16 +6,17 @@ import path from "node:path";
 import { existsSync, mkdirSync } from "node:fs";
 
 const DEFAULT_OPTIONS = {
+  blockLang: "d2",
   compilePath: "static/d2",
   ext: "svg",
   linkPath: "/d2",
 };
 
 /**
- * Plugin for remark that compiles d2 code blocks in markdown.
+ * Plugin for remark that replaces d2 code blocks with compiled d2 images.
  *
- * It creates a directory `${d2Path}/relative path from process.cwd to md file`
- * and creates files named `0.${d2Ext}` in that directory. **It expects a `VFile` with `path`
+ * It creates a directory `${compilePath}/relative path from process.cwd to md file`
+ * and creates files named `0.${ext}` in that directory. **It expects a `VFile` with `path`
  * defined and will not behave as expected if it is not defined.**
  *
  * It replaces the code blocks with language `d2` into image links:
@@ -26,30 +27,37 @@ const DEFAULT_OPTIONS = {
  *
  * \```
  *
- * Becomes:
+ * Is compiled into a file static/d2/some/relative/path/0.svg, and the code block becomes:
  *
- * `![](static/d2/some/relative/path/0.svg)`
+ * `![](/d2/some/relative/path/0.svg)`
  *
  * @param {Object} opts Plugin options
  *
+ * `blockLang` -- which block lang will get replaced, default: `d2`
+ *
  * `compilePath` -- where compiled d2 paths are, default: `static/d2`
  *
- * `ext` -- d2 export file type, default: `svg` (note `png` is significantly slower)
+ * `ext` -- d2 export file type, default: `svg` (note: `png` is slower)
  *
- * `linkPath` -- prepended path before image file when making URL for markdown, e.g. `linkPath: /path/a` -> `[](/path/a/0.svg)`. default: `/d2`
+ * `linkPath` -- dir prepended to relative file path when constructing image URL for markdown,
+ * e.g. `linkPath: /path/ex/` -> `[](/path/ex/some/relative/path/0.svg)`. default: `/d2`
  *
  * @returns Modified AST
  */
 export default function remarkD2(opts) {
   opts = { ...DEFAULT_OPTIONS, ...opts };
-  let count = 0;
 
   return function transformer(tree, file) {
+    // number of d2 images
+    let count = 0;
+
     let imgDir = opts.compilePath;
+    let fPath = "";
     if (file.path !== undefined || file.path !== null) {
       const { dir, name } = path.parse(file.path);
-      const fPath = path.join(dir, name);
+      fPath = path.join(dir, name);
       if (path.isAbsolute(file.path)) {
+        // convert to relative
         imgDir = path.join(imgDir, path.relative(process.cwd(), fPath));
       } else {
         imgDir = path.join(imgDir, fPath);
@@ -62,16 +70,22 @@ export default function remarkD2(opts) {
 
     visit(tree, "code", (node) => {
       const { lang, value } = node;
-      if (!lang || lang !== "d2") return;
+      if (!lang || lang !== opts.blockLang) return;
 
-      const imgCompilePath = path.join(imgDir, `${count++}.${opts.ext}`);
+      const imgCompilePath = path.join(imgDir, `${count}.${opts.ext}`);
 
       const d2 = spawn("d2", ["-", `${imgCompilePath}`]);
       d2.stdin.write(value);
       d2.stdin.end();
 
       node.type = "image";
-      node.url = path.join(opts.linkPath, `${count}.${opts.ext}`);
+      const urlPath = path.join(
+        opts.linkPath,
+        path.relative(process.cwd(), fPath),
+        `${count}.${opts.ext}`,
+      );
+      node.url = urlPath;
+      count++;
     });
     return tree;
   };
